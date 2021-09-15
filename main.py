521462b8300da42b54a5542bace7b52cd4228c45
AndrewCowan2024 <AndrewCowan2024>
2021-09-15 09:13:46 -0500

    main.py

--- main.py
+++ main.py
@@ -14,5 +14,47 @@ def topFiveMovies():
     #print fifth movie
     print("Shrek")
 
+def topFiveSports():
+    #print first sport
+    print("Football")
+    #print second sport
+    print("Golf")
+    #print third sport
+    print("Tennis")
+    #print fourth sport
+    print("Biking")
+    #print fifth sport
+    print("Basketball")
+
+def topFiveVideoGames():
+    #print first Videogame
+    print("Destiny 2")
+    #print second Videogame
+    print("Escape From Tarkov")
+    #print third Videogame
+    print("Apex Legends")
+    #print fourth Videogame
+    print("Titanfall 2")
+    #print fifth Videogame
+    print("Ghost of Tishima")
+
+def topFiveClassesAtHighSchool():
+    #print first Class
+    print("Intro to Programing")
+    #print second Class
+    print("English")
+    #print third Class
+    print("POE")
+    #print fourth Class
+    print("Biology")
+    #print fifth Class
+    print("Gym")
+
 print("these are my top five movies")
 topFiveMovies()
+print("these are my top five sports")
+topFiveSports()
+print("these are my tip five videogames")
+topFiveVideoGames()
+print("these are my top five Classes")
+topFiveClassesAtHighSchool()
