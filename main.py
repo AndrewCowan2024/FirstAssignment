57404d936e4660212b5429c9810f943492ab68bf
AndrewCowan2024 <AndrewCowan2024>
2021-09-24 09:47:52 -0500

    practeced the if

--- main.py
+++ main.py
@@ -1,110 +1,11 @@
-def topFiveMovies():
-    #print the first movie
-    print("Cars 2")
-    
-    #print second movie
-    print("Cars 3")
+def greaterThan10(x):
+    if x > 10:
+        return "x is greater than 10."
 
-    #print third movie
-    print("Cars 1")
+    elif x == 10:
+        return "x equals 10"
 
-    #print fourth movie
-    print("Bee Movie")
+    else:
+        return "x is less than 10."
 
-    #print fifth movie
-    print("Shrek")
-
-def topFiveSports():
-
-    #print first sport
-    print("Football")
-
-    #print second sport
-    print("Golf")
-
-    #print third sport
-    print("Tennis")
-
-    #print fourth sport
-    print("Biking")
-
-    #print fifth sport
-    print("Basketball")
-
-def topFiveVideoGames():
-
-    #print first Videogame
-    print("Destiny 2")
-
-    #print second Videogame
-    print("Escape From Tarkov")
-    
-    #print third Videogame
-    print("Apex Legends")
-
-    #print fourth Videogame
-    print("Titanfall 2")
-
-    #print fifth Videogame
-    print("Ghost of Tishima")
-
-#definea function that outputs your name
-#followed by "is my name"
-def mynameis(turtle):
-
-
-    #output string
-    print(turtle + "is my name.")
-
-
-
-    #run function
-    myNameis("Andrew")
-    myNameis("Chipper")
-    myNameis("Mark")
-
-#define Peraminter
-def timesTwo(chicken):
-
-    #output string
-    print(chicken * 2)
-
-def plusFive(pizza):
-    print(pizza + 5)
-
-def divideTwo(Pasta):
-    print(Pasta / 2)
-
-def madLib(adjective,noun,place,vehicle,pluralnoun,verb,typeOfJob,):
-
-#define function
- def madLib(place,adjective,noun,vehicle,pluralnoun,verb,typeOfJob):
-     print("A long long time ago in a galaxy" + noun + "Yoda was down in " + place + "I was stuck in my" + adjective + "I couldent get out of that dang" + vehicle + "of mine so I had to fight to" + place + "While my dad was off" + typeOfJob + "So that is why I ran away from home")
-
-#run madLib
-madLib("Coconut", "Apple_Bottom_Jeans", "The_boots_With_The_Fur", :def madLib(place):
-     print(place + "food is"+ adjective + "Hello" + place + "Whats your favorite place" + noun + "I dont like that" + pluralnoun + "I like that" +):"Cyber_Truck", Boots, am, Plummer)
-
-
-
-
-print("these are my top five movies")
-
-topFiveMovies()
-
-print("these are my top five sports")
-
-topFiveSports()
-
-print("these are my top five videogames")
-
-topFiveVideoGames()
-
-#run function
-timesTwo(30)
-
-#run function
-plusFive(72)
-
-#run function
-divideTwo(70)
+print(greaterThan10(10))
