6efcad517437a6db56edc1dd3128c65d4d58fd91
AndrewCowan2024 <AndrewCowan2024>
2021-09-27 09:45:07 -0500

    If statement

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-def greaterThan10(x):
-    if x > 10:
-        return "x is greater than 10."
 
-    elif x == 10:
-        return "x equals 10"
-
-    else:
-        return "x is less than 10."
-
-print(greaterThan10(10))
+def define(Word):
+    if word == "Mega":
+        return "Mega means very large and Huge"
+    elif word == "swimming":
+        return "Swimming is the sport or activity of propelling oneself through water using the limbs."
+    elif word == "running":
+        return "Running is the moving of the body"
+    elif word == "turtle":
+        return "Turtle is a animal with a shell"
+    elif word == "cow:":
+        return "A Cow is a 4 legged animal that makes milk"
+    elif word == "milk":
+        return "Milk is a liquild from a Cow"
+    elif word == "water":
+        return "Water is a liquid that is drunk by people"
+    elif word == "axe":
+        return "an axe is a tool to chop down wood"
+    elif word == "sword":
+        return "a sword is a medivel weapon"
+    elif word == "chicken":
+        return "Chicken is a animal"
+    elif word == "beef":
+        return "beef is a type of food"
+print(define("swimming"))
